dc2652cfa268ad276957fad957025cb4e6655191
blocktaco <60831236+blocktaco@users.noreply.github.com>
2022-06-21 16:00:42 -0500

    Add files via upload

--- RenderTextClassHandler.js
+++ RenderTextClassHandler.js
@@ -1,10 +1,12 @@
 const { UltimateTextToImage } = require("ultimate-text-to-image");
 
 async function NewRenderTextAsync(data) {
-    return new UltimateTextToImage(data.text, data.options)
-        .render()
-        .toBuffer()
-        .toString('base64')
+    const TextToImageResult = new UltimateTextToImage(data.text, data.options)
+    return {
+        data: TextToImageResult.render().toBuffer().toString('base64'),
+        width: TextToImageResult.width,
+        height: TextToImageResult.height
+    }
 }
 
 module.exports = {
