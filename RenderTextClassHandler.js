17740089b9ad92a4e6be7d75222482e3b5a06100
blocktaco <60831236+blocktaco@users.noreply.github.com>
2022-06-21 15:43:20 -0500

    Add files via upload

--- RenderTextClassHandler.js
+++ RenderTextClassHandler.js
@@ -2,6 +2,8 @@ const { UltimateTextToImage } = require("ultimate-text-to-image");
 
 async function NewRenderTextAsync(data) {
     console.log(data)
+    console.log(data.options)
+    console.log(data.text)
 }
 
 /*new UltimateTextToImage(`Monkey`, {
